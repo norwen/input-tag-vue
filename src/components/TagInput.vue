a4d5cc37488eb4af0482b3caf127940453ee0bde
norwen <kalininadev@gmail.com>
2024-11-19 14:47:11 +0100

    refactor: move text logic to TextEditor util

--- src/components/TagInput.vue
+++ src/components/TagInput.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="input-container">
-    <DropdownSelect :is-open="dropdownTags.length > 0" :options="dropdownTags" @select="insertTag">
+    <DropdownSelect :is-open="isDropdownOpen" :options="dropdownTags" @select="insertTag">
       <template #control>
-        <div ref="inputRef" class="input-area" contenteditable="true" @input="handleInput"></div>
+        <div
+          ref="editableElement"
+          class="input-area"
+          contenteditable="true"
+          @input="handleInput"
+        ></div>
       </template>
 
       <template #option="{ option }">
@@ -11,110 +16,46 @@
         </div>
       </template>
     </DropdownSelect>
+    {{ activeTag }}
   </div>
 </template>
 
-<script>
-import { ref } from 'vue'
+<script setup>
+import { ref, onMounted, onBeforeUnmount, watch, computed } from 'vue'
 import DropdownSelect from '@/components/DropdownSelect.vue'
+import TextEditor from '@/utils/TextEditor.js'
 
-export default {
-  name: 'HashtagInput',
-  components: { DropdownSelect },
-  setup() {
-    const availableTags = ['#liver', '#heart', '#lung', '#spleen', '#stomach']
-    const inputRef = ref(null)
-    const dropdownTags = ref([])
-    const activeTag = ref('')
+const availableTags = ['#liver', '#heart', '#lung', '#spleen', '#stomach']
+const dropdownTags = ref([...availableTags])
 
-    const saveCursorPosition = () => {
-      const selection = window.getSelection()
-      if (!selection.rangeCount) return null
+const editableElement = ref(null)
+const editor = ref(new TextEditor())
+const activeTag = ref(editor.value.activeTag)
+const isDropdownOpen = computed(() => dropdownTags.value.length > 0 && !!activeTag.value)
 
-      const range = selection.getRangeAt(0)
-      const preCursorRange = range.cloneRange()
-      preCursorRange.selectNodeContents(inputRef.value)
-      preCursorRange.setEnd(range.startContainer, range.startOffset)
+onMounted(() => {
+  console.log(editableElement)
+  editor.value.attachToElement(editableElement.value)
+})
 
-      return preCursorRange.toString().length
-    }
+onBeforeUnmount(() => {
+  editor.value.detachElement()
+})
 
-    const restoreCursorPosition = (offset) => {
-      if (offset === null || inputRef.value === null) return
+watch(activeTag, (newValue, prevValue) => {
+  if (newValue === prevValue) return
+  if (!newValue) dropdownTags.value = []
 
-      const selection = window.getSelection()
-      const range = document.createRange()
-      let charCount = 0
+  dropdownTags.value = availableTags.filter((tag) => tag.includes(newValue))
+})
 
-      const traverseNodes = (node) => {
-        if (node.nodeType === 3) {
-          const nextCharCount = charCount + node.textContent.length
-          if (offset <= nextCharCount) {
-            range.setStart(node, offset - charCount)
-            range.collapse(true)
-            return true // Курсор установлен
-          }
-          charCount = nextCharCount
-        } else {
-          for (let i = 0; i < node.childNodes.length; i++) {
-            if (traverseNodes(node.childNodes[i])) return true
-          }
-        }
-        return false
-      }
+const handleInput = () => {
+  editor.value.handleInput()
+  activeTag.value = editor.value.activeTag
+}
 
-      traverseNodes(inputRef.value)
-      selection.removeAllRanges()
-      selection.addRange(range)
-    }
-
-    // Подсветка хештегов
-    const highlightHashtags = () => {
-      const text = inputRef.value.innerText
-      const highlightedText = text.replace(/#(\w+)/g, `<span style="color:blue;">#$1</span>`)
-
-      const cursorPosition = saveCursorPosition()
-      inputRef.value.innerHTML = highlightedText
-      restoreCursorPosition(cursorPosition)
-    }
-
-    const handleInput = () => {
-      const content = inputRef.value.innerText
-
-      const match = content.match(/#(\w*)$/)
-      if (match) {
-        const tag = match[1]
-        activeTag.value = tag
-        dropdownTags.value = availableTags.filter((t) =>
-          t.toLowerCase().includes(`${tag.toLowerCase()}`),
-        )
-      } else {
-        activeTag.value = ''
-        dropdownTags.value = []
-      }
-
-      highlightHashtags()
-    }
-
-    const insertTag = (tag) => {
-      const content = inputRef.value.innerText
-      const updatedContent = content.replace(/#(\w*)$/, tag + ' ')
-      inputRef.value.innerText = updatedContent
-      highlightHashtags()
-      dropdownTags.value = [] // close dropdown
-      activeTag.value = ''
-
-      const cursorOffset = updatedContent.length
-      restoreCursorPosition(cursorOffset)
-    }
-
-    return {
-      inputRef,
-      dropdownTags,
-      handleInput,
-      insertTag,
-    }
-  },
+const insertTag = (tag) => {
+  editor.value.replaceWord(tag)
 }
 </script>
 
---
+++ src/utils/TextEditor.js
@@ -0,0 +1,102 @@
+export default class TextEditor {
+  constructor(initialText = '') {
+    this.element = null
+    this.activeTag = ''
+  }
+
+  attachToElement(element) {
+    this.element = element
+  }
+
+  detachElement() {
+    this.element = null
+  }
+
+  saveCursorPosition() {
+    const selection = window.getSelection()
+    if (!selection.rangeCount) return null
+
+    const range = selection.getRangeAt(0)
+    const preCursorRange = range.cloneRange()
+    preCursorRange.selectNodeContents(this.element)
+    preCursorRange.setEnd(range.startContainer, range.startOffset)
+
+    return preCursorRange.toString().length
+  }
+
+  restoreCursorPosition(offset) {
+    if (offset === null || this.element === null) return
+
+    const selection = window.getSelection()
+    const range = document.createRange()
+    let charCount = 0
+
+    const traverseNodes = (node) => {
+      if (node.nodeType === 3) {
+        const nextCharCount = charCount + node.textContent.length
+        if (offset <= nextCharCount) {
+          range.setStart(node, offset - charCount)
+          range.collapse(true)
+          return true // Курсор установлен
+        }
+        charCount = nextCharCount
+      } else {
+        for (let i = 0; i < node.childNodes.length; i++) {
+          if (traverseNodes(node.childNodes[i])) return true
+        }
+      }
+      return false
+    }
+
+    traverseNodes(this.element)
+    selection.removeAllRanges()
+    selection.addRange(range)
+  }
+
+  highlightHashtags() {
+    const text = this.element.innerText
+    const highlightedText = text.replace(/#(\w+)/g, `<span style="color:blue;">#$1</span>`)
+
+    const cursorPosition = this.saveCursorPosition()
+    this.element.innerHTML = highlightedText
+    this.restoreCursorPosition(cursorPosition)
+  }
+
+  handleInput() {
+    const selection = window.getSelection()
+    const range = selection.getRangeAt(0)
+    const container = range.startContainer
+    const text = container.textContent || ''
+    const offset = range.startOffset
+
+    const wordAtCursor = this.findWordAtCursor(text, offset)
+
+    if (wordAtCursor.startsWith('#')) {
+      const tag = wordAtCursor.slice(1) // Remove # symbol
+      this.activeTag = tag
+    } else {
+      this.activeTag = ''
+    }
+
+    this.highlightHashtags()
+  }
+
+  findWordAtCursor(text, cursorPosition) {
+    // Find the word boundaries around the cursor
+    const beforeCursor = text.slice(0, cursorPosition).match(/[#\w]*$/)?.[0] || ''
+    const afterCursor = text.slice(cursorPosition).match(/^[#\w]*/)?.[0] || ''
+
+    return beforeCursor + afterCursor
+  }
+  replaceWord(tag) {
+    const content = this.element.innerText
+    const updatedContent = content.replace(/#(\w*)$/, tag + ' ')
+    this.element.innerText = updatedContent
+    this.activeTag = ''
+
+    const cursorOffset = updatedContent.length
+    this.restoreCursorPosition(cursorOffset)
+
+    this.highlightHashtags()
+  }
+}
